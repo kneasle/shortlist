0642817402304f316816bf8402cebdd81e9c8dd3
Kneasle <kneasle@gmail.com>
2020-10-09 16:30:06 +0100

    Document the module and the Shortlist struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,137 @@
+//! A data structure to track the largest items pushed to it with no heap allocations and `O(1)`
+//! amortized time per push.
+//!
+//! ## Features
+//! - Time complexity is `O(1)` per push amortized over every possible input sequence, and 
+//!   `O(log n)` worst case (if the inputs are already sorted)
+//! - No heap allocations except when creating a new `Shortlist`
+//! - 0 dependencies, and only ~150 lines of source code
+//! - 'Safe' versions are provided of functions containg `unsafe` code to prevent heap allocations
+//!
+//! ## The Problem
+//! Suppose that you are running a brute force search over a very large search space, but want to
+//! keep more than just the single best item - for example, you want to find the best 100 items out
+//! of a search of billions options.
+//!
+//! I.e. you want to implement the following function:
+//! ```
+//! fn get_best<T: Ord>(
+//!     big_computation: impl Iterator<Item = T>,
+//!     n: usize
+//! ) -> Vec<T> {
+//!     // Somehow get the `n` largest items produced by `big_computation` ...
+//!     # vec![]
+//! }
+//! ```
+//!
+//! ## A bad solution
+//! The naive approach to this would be to store every item that we searched.  Then once the search
+//! is complete, sort this list and then take however many items we need from the end of the list.
+//! This corresponds to roughly the following code:
+//! ```
+//! fn get_best<T: Ord>(
+//!     big_computation: impl Iterator<Item = T>,
+//!     n: usize
+//! ) -> Vec<T> {
+//!     // Collect all the results into a big sorted vec
+//!     let mut giant_vec: Vec<T> = big_computation.collect();
+//!     giant_vec.sort();
+//!     // Return the last and therefore biggest n items with some iterator magic
+//!     giant_vec.drain(..).rev().take(n).rev().collect()
+//! }
+//!
+//! # // Check that this does in fact do the right thing, albeit very slowly
+//! # assert_eq!(
+//! #     get_best([0, 3, 2, 1, 4, 5].iter().copied(), 3),
+//! #     vec![3, 4, 5]
+//! # );
+//! ```
+//!
+//! But this is massively inefficient in two ways:
+//! - Sorting very large lists is very slow, and we are sorting potentially billions of items that
+//!   we will never need.
+//! - For any decently large search space, storing these items will likely crash the computer by
+//!   making it run out of memory.
+//! 
+//! ## The solution used by this crate
+//! This is where using a `Shortlist` is useful.
+//!
+//! A `Shortlist` is a datastructure that will dynamically keep a 'shortlist' of the best items
+//! given to it so far, with `O(1)` amortized time for pushing new items.  It will also only perform
+//! one heap allocation when the `Shortlist` is created and every subsequent operation will be
+//! allocation free.  Therefore, to the user of this library the code becomes:
+//! ```
+//! use shortlist::Shortlist;
+//!
+//! fn get_best<T: Ord>(
+//!     big_computation: impl Iterator<Item = T>,
+//!     n: usize
+//! ) -> Vec<T> {
+//!     // Create a new Shortlist that will take at most `n` items
+//!     let mut shortlist = Shortlist::new(n);
+//!     // Feed it all the results from `big_computation`
+//!     for v in big_computation {
+//!         shortlist.push(v);
+//!     }
+//!     // Return the shortlisted values as a sorted vec
+//!     shortlist.into_sorted_vec()
+//! }
+//!
+//! # // Check that this does in fact do the right thing
+//! # assert_eq!(
+//! #     get_best([0, 3, 2, 1, 4, 5].iter().copied(), 3),
+//! #     vec![3, 4, 5]
+//! # );
+//! ```
+//!
+//! Or as a one-liner:
+//! ```
+//! use shortlist::Shortlist;
+//!
+//! fn get_best<T: Ord>(big_computation: impl Iterator<Item = T>, n: usize) -> Vec<T> {
+//!     Shortlist::from_iter(n, big_computation).into_sorted_vec()
+//! }
+//!
+//! # // Check that this does in fact do the right thing
+//! # assert_eq!(
+//! #     get_best([0, 3, 2, 1, 4, 5].iter().copied(), 3),
+//! #     vec![3, 4, 5]
+//! # );
+//! ```
+//!
+//! In both cases, the code will make exactly one heap allocation (to reserve space for the
+//! `Shortlist`).
+
 #![deny(clippy::cargo)]
 
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
 
+/// A datastructure that tracks the largest items pushed to it with no heap allocations and `O(1)`
+/// amortized time per push.
+///
+/// A `Shortlist` is a datastructure that will dynamically keep a 'shortlist' of the best items
+/// given to it so far, with `O(1)` amortized time for pushing new items.  It will also only perform
+/// one heap allocation when the `Shortlist` is created and every subsequent operation will be
+/// allocation free.
+///
+/// # Example
+/// Find the top `100` values from 1000 randomly generated integers without storing more than 100
+/// integers on the heap at a time.
+/// ```
+/// use shortlist::Shortlist;
+/// use rand::prelude::*;
+///
+/// // Make a Shortlist and tell it to allocate space for 100 usizes
+/// let mut shortlist: Shortlist<usize> = Shortlist::new(100);
+/// // Push 1000 random values between 0 and 10,000
+/// let mut rng = thread_rng();
+/// for _ in 0..1000 {
+///     shortlist.push(rng.gen_range(0, 10_000));
+/// }
+/// // Consume the shortlist and print its top 100 items in ascending order
+/// println!("{:?}", shortlist.into_sorted_vec());
+/// ```
 #[derive(Debug, Clone)]
 pub struct Shortlist<T> {
     heap: BinaryHeap<Reverse<T>>,
